652a51dbc013bd18bb094e25da1abbc8250e7939
ideateGudy <azonubigoodnews@gmail.com>
2025-02-06 14:09:48 +0100

    back to previous version

--- components/Main.jsx
+++ components/Main.jsx
@@ -4,7 +4,13 @@ import ClaudeRecipe from "./ClaudeRecipe.jsx";
 import { getRecipeFromMistral } from "../ai.js";
 
 export default function Main() {
-  const [ingredients, setIngredients] = React.useState(["hi"]);
+  const [ingredients, setIngredients] = React.useState([
+    "chicken",
+    "all the main spices",
+    "corn",
+    "heavy cream",
+    "pasta",
+  ]);
   const [recipe, setRecipe] = React.useState("");
 
   async function getRecipe() {
